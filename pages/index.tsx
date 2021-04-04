51c7d3e574c8b87379d023017cd3791d2aa4828b
MrElvix <elvismarrugo@gmail.com>
2021-04-04 11:31:47 -0500

    semantic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from 'semantic-ui-react'
+
+
 export default function Home() {
   return (
     <div className="home">
       <h1>Hola Mundo</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   )
 }
