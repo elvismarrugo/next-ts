cff8862c2ffbfa866322273ddc9a812c5de59510
MrElvix <elvismarrugo@gmail.com>
2021-04-07 17:27:56 -0500

    add menu2

--- components/Header/Menu/Menu.tsx
+++ components/Header/Menu/Menu.tsx
@@ -2,6 +2,19 @@ import { Container, Menu, Grid, Icon, Label,  Sidebar  } from "semantic-ui-react
 import Link from "next/link";
 
 export default function MenuWeb() {
+
+
+//   CONFIGURACION	USUARIOS| SEGURIDAD  | CONTACTOS	
+// TRANSACCIONES	VENTAS |COMPRAS|PRODUCTOS |SERVICIOS	
+// FINANCIEROS	TESORERIA	
+// CONTABLES		
+// TRIBUTARIOS	SANCIONES | RENTA | INDUSTRIA Y COMERCIO	
+// NOMINA		
+// REPORTES		
+// CORE		
+// UTILIDADES		
+// SALIR		
+
   return (
     <div className='menu'>
       <Container>
